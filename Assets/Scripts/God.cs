a16f0c5ac22ca70b4ce2e001e3fa254d3b66d1b7
elipmoc <relipmoc101@gmail.com>
2019-09-17 11:23:50 +0900

    画像選択ボタン押したら画像ダイアログ開くように

--- Assets/Scripts/God.cs
+++ Assets/Scripts/God.cs
@@ -1,18 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Infrastructure;
 using UnityEngine;
 
 public class God : MonoBehaviour
 {
+    [SerializeField] ImageSelectButton imageSelectButton;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        imageSelectButton.clicked += () =>
+        {
+            var filePath = new ImgFileDialog().OpenDialogAndGetFilePath();
+            Debug.Log(filePath);
+            //ToDo　ここでfilepathからランク判定して、魚の生成とスコア加算処理する
+        };
     }
 }
---
+++ Assets/Scripts/Infrastructure/ImageSelectButton.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace Infrastructure
+{
+    public class ImageSelectButton : MonoBehaviour
+    {
+        //ボタンが押されたときに呼ばれるイベント
+        public event Action clicked;
+
+        public void OnClick()
+        {
+            clicked?.Invoke();
+        }
+    }
+}
