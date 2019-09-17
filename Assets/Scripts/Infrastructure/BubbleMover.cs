600b53e31806b5179c6cb6e0e9072bcef2b3235d
K-hongou <sosgoole@outlook.jp>
2019-09-18 05:15:28 +0900

    気泡の追加、gameScenesの整理

---
+++ Assets/Scripts/Infrastructure/BubbleMover.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleMover : MonoBehaviour
+{
+    [SerializeField] float y ;
+    [SerializeField]int count;
+    int angle = 0;
+    float amplitude = 0.01f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        angle += 3;
+        amplitude /= 0.995f;
+        transform.position += 
+            new Vector3(Mathf.Cos(Mathf.Deg2Rad * angle) * amplitude,y,0);
+        count-- ;
+        if (count == 0){
+            Destroy (gameObject);
+        }
+    }
+}
